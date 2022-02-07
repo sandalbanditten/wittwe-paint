334835bd9d2218b15af37a713ecb74eac01e2255
sandalbanditten <sandalbanditten@tutanota.com>
2022-02-07 14:31:00 +0100

    Added SDL2

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -xe
 
 CC="gcc"
-CFLAGS="-O2 -Wall -Werror -pedantic -std=c99"
-LIBS=""
+CFLAGS="-O2 -Wall -Werror -pedantic -std=c99 $(sdl2-config --cflags)"
+LIBS="$(sdl2-config --libs) -lm"
 
 $CC $CFLAGS $LIBS $1 -o output
