60b45d0426279a8afcf5684a1b2ecedf50a03a0a
sandalbanditten <sandalbanditten@tutanota.com>
2022-02-07 20:07:15 +0100

    Changed the binary file name

--- build.sh
+++ build.sh
@@ -5,5 +5,6 @@ set -xe
 CC="gcc"
 CFLAGS="-O2 -Wall -Werror -pedantic -std=c99 $(sdl2-config --cflags)"
 LIBS="$(sdl2-config --libs) -lm"
+BIN="wittwe"
 
-$CC $CFLAGS $LIBS $1 -o output
+$CC $CFLAGS $LIBS $1 -o $BIN
